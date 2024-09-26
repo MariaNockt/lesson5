655a19e2d0228ccfd7fc1f443074e6deaea0b359
MariaNockt <m.perevozchi04kova@gmail.com>
2024-09-26 11:17:06 +0400

    Add files via upload

---
+++ REmodule_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = tuple_ = (12, 7, 'драгоценность', True)
+print('Immutable_var', immutable_var) #кортежи нельзя редактировать, так как они не подлежат какому-либо редактированию
+#за исключением списков в них
+mutable_list = ['шприц 5', 'анальгин', 'салфетки спри.', 1, 1, 6]
+mutable_list[1] = 'метоклопроитд'
+print('Mutable_List', mutable_list)
